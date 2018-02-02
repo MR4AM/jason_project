3a9aa0dc1419e8aff750436b97da9a5e9c9913c1
MR4AM <1479856243@qq.com>
2018-02-02 12:06:21 +0800

    飞虎乐购项目

---
+++ gulpfile.js
@@ -0,0 +1,7 @@
+var gulp=require('gulp');
+var sass=require('gulp-sass');
+gulp.task('compileSass',function(){
+    gulp.src('./src/sass/*.scss')
+    .pipe(sass({outputStyle:'expanded'}))  
+    .pipe(gulp.dest('./src/css/'))
+});
